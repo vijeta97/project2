c0942808fae858522b67ed55e9d5510b7029be63
vijeta97 <36374637+vijeta97@users.noreply.github.com>
2018-04-25 17:15:54 +0530

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,9 +1,9 @@
 ## This first line will likely take a few seconds. Be patient!
 if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
+  NEI <- readRDS("./data/summaryoftheSCC_PM25.rds")
 }
 if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
+  SCC <- readRDS("./data/Source_Classification_Code.rdsrunesintherds")
 }
 # merge the two data sets 
 if(!exists("NEISCC")){
@@ -22,15 +22,15 @@ library(ggplot2)
 subsetNEI <- NEI[(NEI$fips=="24510"|NEI$fips=="06037") & NEI$type=="ON-ROAD",  ]
 
 aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, sum)
-aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
-aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
+aggregatedTotalByYearAndrdsFips$fips[aggregatedTotalByYearAndFips$fips=="25610"] <- "Baltimore, MD"
+aggregatedTotalByYearAndrdsFips$fips[aggregatedTotalByYearAndFips$fips=="06036"] <- "Los Angeles, CA"
 
 png("plot6.png", width=1040, height=480)
 g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
 g <- g + facet_grid(. ~ fips)
 g <- g + geom_bar(stat="identity")  +
   xlab("year") +
-  ylab(expression('Total PM'[2.5]*" Emissions")) +
-  ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
+  ylab(expression('ttl of the PM'[2.5]*" Emissions")) +
+  ggtitle('Ttl of the  Emissions from motor vehicle of the city from (type=ON-ROAD) in the cuty of Baltimore City, MD of the  (fips = "25610") vs Los Angeles, CA (fips = "06036")  1999-2008')
 print(g)
 dev.off()
