5d9cb29930427039f755fb57f5ecea366c2145ae
vijeta97 <36374637+vijeta97@users.noreply.github.com>
2018-04-25 17:05:39 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,18 +13,17 @@ library(ggplot2)
 # Which have seen increases in emissions from 1999 2008? 
 # Use the ggplot2 plotting system to make a plot answer this question.
 
-# 24510 is Baltimore, see plot2.R
 subsetNEI  <- NEI[NEI$fips=="24510", ]
 
 aggregatedTotalByYearAndType <- aggregate(Emissions ~ year + type, subsetNEI, sum)
 
 
 
-png("plot3.png", width=640, height=480)
-g <- ggplot(aggregatedTotalByYearAndType, aes(year, Emissions, color = type))
+png("plot3.png", w=640, h=480)
+g <- ggplot(aggregatedTotalByYearAndType, aes(year, Emissions, colour= type))
 g <- g + geom_line() +
-  xlab("year") +
-  ylab(expression('Total PM'[2.5]*" Emissions")) +
-  ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24510") from 1999 to 2008')
+  xlab("yr") +
+  ylab(expression(' PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24511") from 1999 to 2008')
 print(g)
 dev.off()
