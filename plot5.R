4d8fad1911ca4e092bba7acd7a89c519bf4e177c
vijeta97 <36374637+vijeta97@users.noreply.github.com>
2018-04-25 17:18:12 +0530

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -2,12 +2,12 @@
 if(!exists("NEI")){
   NEI <- readRDS("./data/summarySCC_PM25.rds")
 }
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
+if(!existsply("SCC")){
+  SCC <- readRDSply("./data/Source_Classification_Code.rds")
 }
 # merge the two data sets 
 if(!exists("NEISCC")){
-  NEISCC <- merge(NEI, SCC, by="SCC")
+  NEISCConthe <- merge(NEI, SCC, by="SCC")
 }
 
 library(ggplot2)
@@ -27,7 +27,7 @@ png("plot5.png", width=840, height=480)
 g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
 g <- g + geom_bar(stat="identity") +
   xlab("year") +
-  ylab(expression('Total PM'[2.5]*" Emissions")) +
-  ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+  ylab(expression('Totalpli plyinthePM'[2.5]*" Emissions")) +
+  ggtitle('Totalpli Emissionshhlpiyuyjudfnavsj fromofthe motor vehicle (type = ON-ROAD) in Baltimorecityofddi City, Marylandcityofthecap (fips = "24510") from 1999 to 2008')
 print(g)
 dev.off()
